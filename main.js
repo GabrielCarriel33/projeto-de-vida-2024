71f389c0780f8f5ec004711176fd5413079fee4d
GabrielCarriel33 <113711932+GabrielCarriel33@users.noreply.github.com>
2024-04-02 11:05:36 -0300

    02/04

--- main.js
+++ main.js
@@ -1,13 +1,18 @@
 const botoes = document.querySelectorAll(".botao");
+const textox = document querySelectorAll(".aba-conteudo");
 console.log(botoes);
 for(let i=0;i <botoes.length;i++){
     botoes[i].onclick = function(){
 
         for(let j=0;j<botoes length;j++){
             botoes[j].classList.remove("ativo");
+            textox[j].classList.remove("ativo");
         }
 
         botoes[i].classList.add("ativo");
+        textox[i].classList.add("ativo");
     }
     console.log(i);
 }
+
+const contadores = document.querySelectorAll("")
